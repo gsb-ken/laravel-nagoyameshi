6b929f443cf9da78c0e972be73cd6363a57aa7f6
kenta hoshi <hyuma_h@hotmail.co.jp>
2025-08-06 21:05:56 +0900

    fix

--- terraform/modules/ecs/task_definition_migrate/main.tf
+++ terraform/modules/ecs/task_definition_migrate/main.tf
@@ -31,7 +31,7 @@ resource "aws_ecs_task_definition" "migrate" {
         { name = "DB_PASSWORD",   value = var.db_password }
       ]
       # command = ["sh", "-c", "php artisan migrate --force 2>&1; echo ExitCode:$?; sleep 60"]
-      command = ["sh","-c","mysql -h $$DB_HOST -u $$DB_USERNAME -p$$DB_PASSWORD -e 'SELECT NOW();'; sleep 60"]
+      command = ["sh","-c","mysql -h $$DB_HOST -u $$DB_USERNAME -p$$DB_PASSWORD -e 'SELECT NOW();'; sleep 80"]
       logConfiguration = {
         logDriver = "awslogs"
         options = {
